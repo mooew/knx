cc63a6915c30d6921cf4b494ebf0f80e2c1434fb
mooew <wietse.gh@gmail.com>
2017-12-11 22:53:00 +0100

    mode selector

--- knx.js
+++ knx.js
@@ -4,8 +4,10 @@ var dp1 = new knx.Datapoint({ga: '0/3/8', dpt: 'DPT5.001'});
 var dp2 = new knx.Datapoint({ga: '0/3/2', dpt: 'DPT9.001'});
 var ext_temp = new knx.Datapoint({ga: '0/3/0', dpt: 'DPT9.001'});
 var comf = new knx.Datapoint({ga: '0/3/3', dpt: 'DPT9.001'});
+var mode_fb = new knx.Datapoint({ga: '0/3/4', dpt: 'DPT20.102'});
+var mode = new knx.Datapoint({ga: '0/3/5', dpt: 'DPT20.102'});
 
-var ets = {dp1, dp2, ext_temp, comf}
+var ets = {dp1, dp2, ext_temp, comf, mode_fb, mode}
 
 
 var connection = knx.Connection({
@@ -37,4 +39,7 @@ var connection = knx.Connection({
   }
 });
 
+
+
   module.exports.ets = ets
+  //module.exports.connection = connection
